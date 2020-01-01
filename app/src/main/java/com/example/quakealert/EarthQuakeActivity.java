78bee119478d036ecb80629cb7759a943da4fb2f
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-01-01 13:42:51 +0100

    Added a QueryUtils class

--- app/src/main/java/com/example/quakealert/EarthQuakeActivity.java
+++ app/src/main/java/com/example/quakealert/EarthQuakeActivity.java
@@ -15,17 +15,7 @@ public class EarthQuakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake);
 
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("1.0", "Nasarrawa", "1-1-2019"));
-        earthquakes.add(new Earthquake("2.0", "Benin", "1-2-2019"));
-        earthquakes.add(new Earthquake("3.0", "Sapele", "1-3-2019"));
-        earthquakes.add(new Earthquake("4.0", "Auchi", "1-4-2019"));
-        earthquakes.add(new Earthquake("5.0", "Abuja", "1-5-2019"));
-        earthquakes.add(new Earthquake("6.0", "Kebbi", "1-6-2019"));
-        earthquakes.add(new Earthquake("7.0", "Katsina", "7-1-2019"));
-        earthquakes.add(new Earthquake("8.0", "Niger", "1-8-2019"));
-        earthquakes.add(new Earthquake("9.0", "Kaduna", "1-9-2019"));
-        earthquakes.add(new Earthquake("3.5", "Calabar", "1-10-2019"));
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this, earthquakes);
 
