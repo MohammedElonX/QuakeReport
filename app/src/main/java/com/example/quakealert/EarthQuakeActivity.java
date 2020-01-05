96c5fb6a3b9e92d614ff16269db15b9c030b6475
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-01-05 21:28:51 +0100

    Extend AsynchTask and overRide methods

--- app/src/main/java/com/example/quakealert/EarthQuakeActivity.java
+++ app/src/main/java/com/example/quakealert/EarthQuakeActivity.java
@@ -5,14 +5,22 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.StringBuilderPrinter;
 import android.view.View;
 import android.widget.ListView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class EarthQuakeActivity extends AppCompatActivity {
@@ -26,7 +34,7 @@ public class EarthQuakeActivity extends AppCompatActivity {
 
         ArrayList<Earthquake> earthquakes;
 
-        EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this, earthquakes);
+        EarthquakeAdapter earthquakeAdapter = EarthquakeAsyncTask.execute();
 
         ListView earthquakeListView = findViewById(R.id.earthquake_list);
 
@@ -66,9 +74,42 @@ public class EarthQuakeActivity extends AppCompatActivity {
         protected String doInBackground(URL... urls) {
             //Make Url object on API
             URL url = createUrl(REQUEST_URL);
+
+            //Make HTTP Request to recieve JSON
+            String Json = "";
+            Json = makeHTTPrequest(url);
             return null;
         }
 
+        private String makeHTTPrequest(URL url){
+            String JsonRequest = "";
+            HttpURLConnection httpURLConnection = null;
+            InputStream inputStream = null;
+            try {
+                URLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
+                urlConnection.connect();
+                inputStream = urlConnection.getInputStream();
+                JsonRequest = readFromStream(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private String readFromStream(InputStream inputStream) throws IOException {
+            StringBuilder output = new StringBuilder();
+            if (inputStream != null){
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while(line != null){
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+            return output.toString();
+        }
 
         private URL createUrl(String url){
             URL Url = null;
